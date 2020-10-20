259a20f9b568596591aaa9d8323e2512fa8f1c5a
hasan <burhannur19@gmail.com>
2020-10-20 20:36:39 +0700

    update landing

---
+++ resources/views/layout-home/footer.blade.php
@@ -0,0 +1,62 @@
+<footer>
+    <div class="footer">
+    <div class="container">
+        <div class="image-footer pb-4">
+            <img src="/image/icon-footer.svg" alt="">
+        </div>
+        <div class="row footer-content">
+            <div class="col-md-6">
+                <ul>
+                    <li class="ftr-tps">Sinau tani adalah sebuah website penyedia kursus online gratis, yang berbasis di Yogyakarta, Indonesia dan didirikan pada Oktober 2020</li>
+                    <li class="ftr-tipis">Jika anda memiliki ide, kritik atau saran,bisa menghubungi hotline kami di :</li>
+                    <li class="ftr-biasa">(+62)81234567890</li>
+                    <li class="ftr-biasa">sales-sitani-idn@gmail.com</li>
+                    <li class="ftr-biasa">kerjasama-sitani-idn@gmail.com</li>
+                </ul>
+            </div>
+            <div class="col-md-2 bantuan-link">
+                <ul>
+                    <li>
+                        <strong>Menu</strong>
+                    </li>
+                    <li class="ftr-tipis">Kategori :</li>
+                    <li>
+                        <a href="" class="ftr-biasa">Pertanian</a>
+                    </li>
+                    <li>
+                        <a href="" class="ftr-biasa">Perkebunan</a>
+                    </li>
+                    <li>
+                        <a href="" class="ftr-biasa">Pertanian modern</a>
+                    </li>
+                    <li>
+                        <a href="" class="ftr-biasa">Testimoni</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <ul>
+                    <li>
+                        <strong>Kantor</strong>
+                    </li>
+                    <li class="ftr-tipis">
+                        Yogyakarta, Indonesia
+                    </li>
+                    <li>
+                        <strong>Support dan Donasi</strong>
+                    </li>
+                    <li class="ftr-tipis">
+                        (+62)81234567890
+                    </li>
+                    <li class="ftr-tipis">
+                        donasi_sitani@gmail.com
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer-end d-flex justify-content-center">
+    <h5 class="ending mt-2">Copyright sitani 2020</h5>
+</div>
+</footer>
---
+++ resources/views/layout-home/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-dark navbar-font basic-color" >
+    <div class="container">
+        <a class="navbar-brand icon" href="#">
+            <img src="/image/icon.svg" width="80" height="80" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active mr-3">
+                    <a class="nav-link" href="#">TEMUKAN</a>
+                </li>
+                <li class="nav-item dropdown mr-3 active">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        KATEGORI KELAS
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item active mr-3">
+                    <a class="nav-link" href="#">KENAPA HARUS SI TANI?</a>
+                </li>
+                <li class="nav-item active mr-3">
+                    <a class="nav-link" href="#">SARAN</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">TENTANG</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
