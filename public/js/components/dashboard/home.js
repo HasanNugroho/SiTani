9d3df0baa2340dbdb0546595062d8a08267462cd
pudy <satriadevops@gmail.com>
2020-10-20 22:38:03 +0700

    progress interface backend 80%

---
+++ public/js/components/dashboard/home.js
@@ -0,0 +1,39 @@
+export const home = `<div class="row container-fluid">
+  				<h3 class="mt-2">Dashboard</h3>
+
+  				<div class="col-4">
+  					<div class="card text-white">
+
+					  <img src="/assets/image/banner/user.png" class="card-img" alt="...">
+					  <div class="card-img-overlay d-flex">
+					  	<div class="mt-auto ml-auto text-right">					  		
+					    <h5 class="card-title m-auto d-block">USER</h5>
+					    <p class="card-text m-auto d-block">Terdaftar 10 User </p>
+					  	</div>
+					  </div>
+					</div>
+  				</div>
+  				<div class="col-4">
+  					<div class="card text-white">
+					  <img src="/assets/image/banner/tanggapan.png" class="card-img" alt="...">
+					  <div class="card-img-overlay d-flex">
+					  	<div class="mt-auto ml-auto text-right">					  		
+					    <h5 class="card-title m-auto d-block">USER</h5>
+					    <p class="card-text m-auto d-block">Terdaftar 10 User </p>
+					  	</div>
+					  </div>
+					</div>
+  				</div>
+  				<div class="col-4">
+  					<div class="card text-white">
+					  <img src="/assets/image/banner/komen.png" class="card-img" alt="...">
+					  <div class="card-img-overlay d-flex">
+					  	<div class="mt-auto ml-auto text-right">					  		
+					    <h5 class="card-title m-auto d-block">USER</h5>
+					    <p class="card-text m-auto d-block">Terdaftar 10 User </p>
+					  	</div>
+					  </div>
+					</div>
+  				</div>
+  			</div>
+  		</div>`;
